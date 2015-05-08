6cc3d9d30b0cd1dd2905ac5a84d1b4f03104e600
AndreaGuzman1 <andreaviviana.guzman@gmail.com>
2015-05-08 11:58:48 -0300

    cambios por la modificacion de Usuario a  Empleado

--- RegalosEmpleados/test/unit/regalosempleados/UsuarioSpec.groovy
+++ RegalosEmpleados/test/unit/regalosempleados/UsuarioSpec.groovy
@@ -6,7 +6,7 @@ import spock.lang.Specification
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
-@TestFor(Usuario)
+@TestFor(Empleado)
 class UsuarioSpec extends Specification {
 
     def setup() {
