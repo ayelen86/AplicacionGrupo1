8eb3e98a7200518c67afae6b5aeb1fb762a93bfa
ayelen86 <ayelen86>
2015-05-14 10:01:15 -0300

    Agregamos Servicios

--- RegalosEmpleados/grails-app/controllers/regalosempleados/GestionarController.groovy
+++ RegalosEmpleados/grails-app/controllers/regalosempleados/GestionarController.groovy
@@ -3,11 +3,11 @@ package regalosempleados
 import grails.converters.JSON
 import org.springframework.web.servlet.ModelAndView
 import grails.plugin.springsecurity.annotation.Secured
-import grails.plugin.springsecurity.SpringSecurityService;
-
+import grails.plugin.springsecurity.SpringSecurityService
+import regalosempleados.GestionarService
 
 class GestionarController {
-	
+	def gestionarService
 	def springSecurityService
 
 	@Secured(['ROLE_ADMIN_GENERAL','ROLE_ADMIN_EMPRESA','ROLE_EMPLEADO'])
@@ -17,20 +17,13 @@ class GestionarController {
 
 	@Secured(['ROLE_ADMIN_GENERAL','ROLE_ADMIN_EMPRESA','ROLE_EMPLEADO'])
 	def createListAnio(){
-		def ArrayList<Anio> anios=new ArrayList<Anio>()
-		for (int i= 1942; i<2100;i++){
-			Anio anio = new Anio()
-			anio.anio = i
-			anios.add(anio)
-		}
-		return anios
+		return gestionarService.listarAnios()
 	}
 	
 	@Secured(['ROLE_ADMIN_EMPRESA','ROLE_EMPLEADO'])
 	def verCumpleanieros(){
-		def user = springSecurityService.currentUser
-		def empresa=user.empresa		
-		def empleados = empresa.empleados		
+				
+		def empleados = gestionarService.empleadosDeUnaEmpresa()	
 		def cumpleanieros = new ArrayList<Empleado>()
 		Date mesActual = new Date()
 		for(def empleado : empleados){
@@ -43,26 +36,26 @@ class GestionarController {
 	
 	@Secured(['ROLE_ADMIN_EMPRESA'])
 	def regaloGuardado() {
-		def regalo = new Regalo()
-		regalo.idMercadolibre = params.regalo
-		regalo.anio = new Integer(params.anio)		
-		def empleado = Empleado.findById(params.empleadoId)
-		empleado.addToRegalos(regalo).save(flush: true)
+		
+		gestionarService.guardarRegalo(params.regalo,params.anio,params.empleadoId)
+//		def regalo = new Regalo()
+//		regalo.idMercadolibre = params.regalo
+//		regalo.anio = new Integer(params.anio)		
+//		def empleado = Empleado.findById(params.empleadoId)
+//		empleado.addToRegalos(regalo).save(flush: true)
 	}
 	
 	@Secured(['ROLE_ADMIN_EMPRESA','ROLE_EMPLEADO'])
 	def verRegalo() {
-		def user = springSecurityService.currentUser
-		def empresa=user.empresa
-		def listaUsuarios = empresa.empleados		
+		
+		def listaUsuarios = gestionarService.empleadosDeUnaEmpresa()		
 		return new ModelAndView("verRegalo", [empleados: listaUsuarios])
 	}
 
 	@Secured(['ROLE_ADMIN_EMPRESA'])
 	def seleccionarRegalo (){
-		def user = springSecurityService.currentUser
-		def empresa=user.empresa
-		def listaUsuarios = empresa.empleados		
+		
+		def listaUsuarios = gestionarService.empleadosDeUnaEmpresa()		
 		def listaAnios=createListAnio()			
 		Date d = new Date()
 		return new ModelAndView("seleccionarRegalo", [empleados: listaUsuarios, anios: listaAnios])
